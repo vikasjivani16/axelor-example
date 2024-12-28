f35a3b3a90b66cd96c57f1250f487e52a8a0befd
vikas jivani <vikasjivani13@gmail.com>
2024-12-28 17:24:13 +0100

    Implement Installation page

--- app/installation/page.tsx
+++ app/installation/page.tsx
@@ -1,5 +1,7 @@
 import styles from "../components/Layout.module.css";
 import Sidebar from "../components/Sidebar";
+import int_styles from "../introduction.module.css";
+
 
   const Installation = () => {
   
@@ -10,9 +12,108 @@ import Sidebar from "../components/Sidebar";
         {/* Content Area */}
         <div className={styles.content}>
       
-            <p>Installation Page</p>
+        <h1 className={int_styles.header}> <b>Setup guide</b></h1>
+        <p>Depending on the intended use case, there are multiple ways to install Axelor.
+          For developers of the Axelor community alike, 
+          the preferred way is to perform a source install (running Axelor from the source code).</p>
+        
+          <br/>
+          <h1 className={int_styles.header2}> <b>Required Software</b></h1>
           
+          <ol className={int_styles.ol}>
+            <li className={int_styles.li}>1. Java 11</li>
+            <li className={int_styles.li}>2. Postgres 13.0 or above version</li>
+            <li className={int_styles.li}>3. Tomcat 9.0</li>
+          </ol>
+
+          <br/>
+          <h1 className={int_styles.header2}> <b>Step 1: Codebase Setup</b></h1>
+          
+          <p>Clone root axelor repository from Github. </p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+              $  git clone https://github.com/axelor/open-suite-webapp.git         
+            </code>
+          </pre>
+
+          <br/>
+          <p><b>Note:</b> Re-name folder <b>&quot;open-suite-webapp&quot;</b> to <b>&quot;axelor-erp&quot;</b>.</p>
+          
+          <p>Change directory to <b>&quot;axelor-erp/module/axelor-open-suite&quot;.</b></p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+              $  cd axelor-erp/module/axelor-open-suite           
+            </code>
+          </pre>
+
+
+          <br/>
+          <p>Clone module repository from Github.</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+              $  git clone https://github.com/axelor/axelor-open-suite.git           
+            </code>
+          </pre>
+
+          <br/>
+          <p><b>Note:</b> Follow below steps for re-location module derectory.</p>
+
+          <p>Project structure look like <b>&quot;axelor-erp/module/axelor-open-suite/axelor-open-suite/*&quot;</b> </p>
+          <p>Change like <b>&quot;axelor-erp/module/axelor-open-suite/*&quot;</b>. delete one sub-folder <b>&quot;axelor-open-suite&quot;</b></p>
+
+
+          <br/>
+          <h1 className={int_styles.header2}> <b>Step 2: Create Database & Link with Project</b></h1>
+
+          <p>Postgres SQL used in this project for setup database.</p>
+          <p>Open configuration file <b>&quot;axelor-erp/src/main/resources/axelor-config.properties&quot;</b>.</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+            db.default.driver = org.postgresql.Driver <br/>
+            db.default.ddl = update <br/>
+            db.default.url = jdbc:postgresql://localhost:5432/<b>axelor_bd</b> <br/>
+            db.default.user = <b>postgres</b> <br/>
+            db.default.password = <b>password</b> <br/>    
+            </code>
+          </pre>
+
+          <br/>
+          <h1 className={int_styles.header2}> <b>For Linux</b></h1>
+          <p>Open terminal from root folder <b>&quot;axelor-erp&quot;</b> and build the project (It will take around 5-10 min for first time)</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+            $ ./gradlew clean classes build -x test cleanEclipse eclipse
+            </code>
+          </pre>
+          <p>Start server</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+            $ ./gradlew --no-daemon run
+            </code>
+          </pre>
+
+          <br/>
+          <h1 className={int_styles.header2}> <b>For Windows</b></h1>
+          <p>Open terminal from root folder <b>&quot;axelor-erp&quot;</b> and build the project (It will take around 5-10 min for first time)</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+            $ gradlew.bat clean classes build -x test cleanEclipse eclipse
+            </code>
+          </pre>
+          <p>Start server</p>
+          <pre className={int_styles.pre}>
+            <code className={int_styles.code}>
+            $ gradlew.bat --no-daemon run
+            </code>
+          </pre>
+
         </div>
+
+
+        <div className={styles.sidebar2}>
+
+        </div>
+
       </div>
     );
   };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Sidebar from "./components/Sidebar";
 import styles from "./components/Layout.module.css";
-
+import int_styles from "./introduction.module.css";
 
 export default function Home() {
   return (
@@ -8,8 +8,30 @@ export default function Home() {
       <Sidebar />
       {/* Content Area */}
       <div className={styles.content}>
+          <h1 className={int_styles.header}> <b>Overview </b></h1>
           <p>Welcom to learn Axelor real time example...</p>
+          
+
+              <pre className={int_styles.pre}>
+                <code className={int_styles.code}>
+                
+                  &lt;?xml version=&quot;1.0&quot; encoding=&quot;UTF-8&quot; standalone=&quot;yes&quot;?&gt; <br/>
+                  &lt;object-views xmlns=&quot;http://axelor.com/xml/ns/object-views&quot;<br/>
+                    xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot;<br/>
+                    xsi:schemaLocation=&quot;http://axelor.com/xml/ns/object-views<br/>
+                    https://axelor.com/xml/ns/object-views/object-views_7.2.xsd&quot;&gt;<br/>
+
+                    &lt;!-- views definitions here --&gt;<br/>
+
+                  &lt;/object-views&gt;<br/>
+                  
+                </code>
+              </pre>
+        
       </div>
+      <div className={styles.sidebar2}>
+        qq
+        </div>
     </div>
   );
 }
